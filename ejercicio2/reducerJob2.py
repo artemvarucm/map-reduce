9f97a892f9a87f5a38d6d5b639e6661f4e86756f
Artem <artemvar@ucm.es>
2025-03-20 18:12:59 +0100

    fix numeros

--- ejercicio2/reducerJob2.py
+++ ejercicio2/reducerJob2.py
@@ -7,6 +7,7 @@ list = []
 for line in sys.stdin:
     line = line.replace('\n', '')
     url, cnt = line.split('\t')
+    cnt = int(cnt)
     # insert record in list sorted by f
     bisect.insort(list, {"url": url, "cnt": cnt}, key=lambda x: x["cnt"])
     # truncate list to top K
