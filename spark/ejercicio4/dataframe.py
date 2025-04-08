d978cd71924bb8f5bb1ebccb7e6d92b616bfc50e
Artem <artemvar@ucm.es>
2025-04-09 01:22:17 +0200

    ejercicio 4 hecho

--- spark/ejercicio4/dataframe.py
+++ spark/ejercicio4/dataframe.py
@@ -9,13 +9,8 @@ spark = SparkSession.builder \
 
 sc = spark.sparkContext
 df = spark.read.csv(sys.argv[1], header=True, sep=",")
-# asignar range despues de la media
-df = df.withColumn("rating-label", concat(lit("Range-" ), ceil(col("rating"))))
-df = df.replace("Range-0", "Range-1")
-df.show(5)
-
-df.groupBy("rating-label")\
-    .agg({"rating": "avg"})\
-    .withColumnRenamed("avg(rating)", "Average")\
-    .select("rating-label", "Average")\
+df = df.groupBy("movieId").agg({"rating": "avg"}).withColumnRenamed("avg(rating)", "Average")
+df = df.withColumn("rating-label", concat(lit("Range-" ), ceil(col("Average"))))
+df = df.replace("Range-0", "Range-1") # el valor 0.0 pertenece al rango 1
+df.select("movieId", "rating-label", "Average")\
     .write.csv(sys.argv[2], header=True, mode="overwrite")
--- spark/ejercicio4/rdd.py
+++ spark/ejercicio4/rdd.py
@@ -1,15 +1,26 @@
 from pyspark.sql import SparkSession
 import sys
 
+def mapRatingToRange(rating):
+    if rating <= 1:
+        return "Range-1"
+    elif rating <= 2:
+        return "Range-2"
+    elif rating <= 3:
+        return "Range-3"
+    elif rating <= 4:
+        return "Range-4"
+    else:
+        return "Range-5"
+
 spark = SparkSession.builder \
-    .appName("Ej5") \
+    .appName("Ej4") \
     .getOrCreate()
 
 sc = spark.sparkContext
-rdd = spark.read.csv(sys.argv[1], header=True, sep=";").rdd
-rdd = rdd.filter(lambda x: x[4] != None)
-rdd = rdd.map(lambda x: (x[3], (float(x[4]), 1)))
+rdd = spark.read.csv(sys.argv[1], header=True, sep=",").rdd
+rdd = rdd.map(lambda x: (x[1], (float(x[2]), 1)))
 rdd = rdd.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))\
-    .map(lambda x: (x[0], x[1][0] / x[1][1]))
+    .map(lambda x: (x[0], mapRatingToRange(x[1][0] / x[1][1]), x[1][0] / x[1][1]))
 
 rdd.saveAsTextFile(sys.argv[2])
